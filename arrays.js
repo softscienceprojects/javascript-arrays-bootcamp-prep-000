8e9781bab18fce34b4eef1eda7750deec8a7550b
erin johnson <erin.nicole.johnson@gmail.com>
2017-10-01 08:57:56 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+// [e, ...a]
+};
+function destructivelyAddElementToBeginningOfArray(array, element) {
+array.unshift(element)
+return array
+};
+
+function addElementToEndOfArray(array, element) {
+  //[a..., e]
+};
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+};
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1);
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  array.slice(array.length-1)
+  return array
+}
